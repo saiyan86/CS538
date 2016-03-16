c8fa03f77836b49e4bc565ff97525a314adb25e4
Yongli Chen <12708785@qq.com>
2016-03-15 21:24:52 -0500

    1st

---
+++ firewall.py
@@ -0,0 +1,22 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.revent import *
+from pox.lib.util import dpidToStr
+from pox.lib.addresses import EthAddr
+from collections import namedtuple
+import os
+
+log = core.getLogger()
+
+class Firewall (EventMixin):
+
+    def __init__ (self):
+        self.listenTo(core.openflow)
+        log.debug("Enabling Firewall Module")
+
+    def _handle_ConnectionUp (self, event):
+        ''' Add your logic here ... '''
+        log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
+
+def launch ():
+    core.registerNew(Firewall)
